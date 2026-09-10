c2aaddff19d6433f52a80e97317317715ab03318
AdarshGrewal <adarshgrewal@gmail.com>
2026-09-10 09:44:19 +0530

    onyx: Set LED overlays only for CN hwc
    Change-Id: I8215f6f81b8811d8c16862d09d28dd3aebdf38ac

--- device.mk
+++ device.mk
@@ -328,8 +328,10 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayOnyx \
     TelephonyOverlayOnyx \
     FrameworkOverlayOnyx \
+    FrameworkOverlayOnyxCN \
     FrameworkOverlayOnyxGL \
     LineageSDKOverlayOnyx \
+    LineageSDKOverlayOnyxCN \
     LineageSettingsOverlayOnyx \
     NfcOverlayOnyx \
     SettingsOverlayOnyx \
