2f7a00c3fb7b16858ff7d333144a0aebd5c30c48
Matt Buckley <mattbuckley@google.com>
2026-09-17 16:26:17 +0000

    onyx: Enable adpf cpu hint session for SurfaceFlinger and HWUI  * Make HWUI disabled by default for vendors  * Explicitly enable in supported devices  * Consolidate flags for HWUI and SF in hardware code
    Bug: b/261628396
    Test: manual
    Change-Id: I7352f933993770498d3403aeb4e45fd3f1afab88
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -168,6 +168,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LunarisDolby
 
+# Enable adpf cpu hint session for SurfaceFlinger and HWUI
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_adpf_cpu_hint=true \
+    debug.hwui.use_hint_manager=true
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
